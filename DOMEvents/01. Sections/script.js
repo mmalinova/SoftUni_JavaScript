40aa300fd7f9d3f542f61af4b018fb51d6b0d684
mmalinova <malinova29@gmail.com>
2021-02-12 22:02:20 +0200

    Add Sections and Time-Converter.

--- DOMEvents/01. Sections/script.js
+++ DOMEvents/01. Sections/script.js
@@ -1,3 +1,12 @@
 function create(words) {
-   console.log('TODO:...');
+   const div = document.getElementById('content');
+   for (const str of words) {
+      let myDiv = document.createElement('div');
+      let paragraph = document.createElement('p');
+      paragraph.textContent = str;
+      paragraph.style.display = 'none';
+      myDiv.appendChild(paragraph);
+      myDiv.addEventListener('click', function(){paragraph.style.display = 'block';}, false);
+      div.appendChild(myDiv);
+   }
 }
--- DOMEvents/02. Time-Converter/solution.js
+++ DOMEvents/02. Time-Converter/solution.js
@@ -1,4 +1,59 @@
 function attachEventsListeners() {
+    let days = 0;
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
 
-    console.log('TODO:...');
+    const daysType = document.getElementById('days');
+    const daysBtn = document.getElementById('daysBtn');
+    daysBtn.addEventListener('click', day, false);
+    const hoursType = document.getElementById('hours');
+    const hoursBtn = document.getElementById('hoursBtn');
+    hoursBtn.addEventListener('click', hour, false);
+    const minutesType = document.getElementById('minutes');
+    const minutesBtn = document.getElementById('minutesBtn');
+    minutesBtn.addEventListener('click', minute, false);
+    const secondsType = document.getElementById('seconds');
+    const secondsBtn = document.getElementById('secondsBtn');
+    secondsBtn.addEventListener('click', second, false);
+
+    function day() {
+        days = daysType.value;
+        hours = days * 24;
+        minutes = hours * 60;
+        seconds = minutes * 60;
+        hoursType.value = hours;
+        minutesType.value = minutes;
+        secondsType.value = seconds;
+    }
+
+    function hour() {
+        hours = hoursType.value;
+        days = hours / 24;
+        minutes = hours * 60;
+        seconds = minutes * 60;
+        daysType.value = days;
+        minutesType.value = minutes;
+        secondsType.value = seconds;
+    }
+
+    function minute() {
+        minutes = minutesType.value;
+        hours = minutes / 60;
+        days = hours / 24;
+        seconds = minutes * 60;
+        hoursType.value = hours;
+        daysType.value = days;
+        secondsType.value = seconds;
+    }
+
+    function second() {
+        seconds = secondsType.value;
+        minutes = seconds / 60;
+        hours = minutes / 60;
+        days = hours / 24;
+        hoursType.value = hours;
+        minutesType.value = minutes;
+        daysType.value = days;
+    }
 }
