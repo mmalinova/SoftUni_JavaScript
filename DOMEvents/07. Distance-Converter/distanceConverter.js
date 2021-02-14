ea6521e1b56ef2ddfdeeebd28b82a69d551c1ff8
mmalinova <malinova29@gmail.com>
2021-02-14 20:31:55 +0200

    Add last three tasks.

--- DOMEvents/07. Distance-Converter/distanceConverter.js
+++ DOMEvents/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,25 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const convertBtn = document.getElementById('convert');
+
+    convertBtn.addEventListener('click', () => {
+        const input = Number(document.getElementById('inputDistance').value);
+        const inputUnit = document.getElementById('inputUnits').value;
+        const outputUnit = document.getElementById('outputUnits').value;
+
+        let units = {
+            'km': 1000,
+            'm': 1,
+            'cm': 0.01,
+            'mm': 0.001,
+            'mi': 1609.34,
+            'yrd': 0.9144,
+            'ft': 0.3048,
+            'in': 0.0254,
+        }; 
+
+        let meters = input * units[inputUnit];
+        let result = meters / units[outputUnit];
+
+        document.getElementById('outputDistance').value = result;
+    });
 }
--- DOMEvents/09. JavaScript-Quiz/solution.js
+++ DOMEvents/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-  //TODO...
+  const div = document.getElementById('quizzie');
+
+  let count = 0;
+  let result = 0;
+
+  div.addEventListener('click', (e) => {
+    if (e.target.tagName === 'P') {
+      if (e.target.textContent === 'onclick'
+        || e.target.textContent === 'JSON.stringify()'
+        || e.target.textContent === 'A programming API for HTML and XML documents') {
+        result++;
+      }
+      let currentSection = e.target.parentElement.parentElement.parentElement.parentElement;
+      currentSection.style.display = 'none';
+      if (count < 2) {
+        let section = document.getElementsByClassName('hidden')[count++];
+        section.style.display = 'block';
+      } else {
+        let output = document.getElementById('results');
+        output.style.display = 'block';
+        if (result === 3) {
+          output.children[0].children[0].textContent = 'You are recognized as top JavaScript fan!';
+        } else {
+          output.children[0].children[0].textContent = 'You have ' + `${result}` + ' right answers';
+        }
+      }
+    }
+  });
 }
