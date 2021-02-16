a60e778175b257979a165e9a9d2cddb5bcc33bea
mmalinova <malinova29@gmail.com>
2021-02-16 18:19:41 +0200

    Add last tasks.

--- UnitTestingErrorHandling/06.Dynamic-Validation/app.js
+++ UnitTestingErrorHandling/06.Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    // TODO
+    let input = document.getElementById('email');
+
+    input.addEventListener('change', () => {
+        let regex = /^[a-zA-Z]+@[a-zA-Z]+.[a-zA-Z]+$/gm;
+        if (!regex.test(input.value)) {
+            input.classList.add('error');
+        } else {
+            input.removeAttribute('class');
+        }
+    });
 }
--- UnitTestingErrorHandling/07.Form-Validation/formValidation.js
+++ UnitTestingErrorHandling/07.Form-Validation/formValidation.js
@@ -1,3 +1,63 @@
 function validate() {
-    // TODO
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const confirmPass = document.getElementById('confirm-password');
+    const companyInfo = document.getElementById('companyInfo');
+    const companyNumber = document.getElementById('companyNumber');
+
+    let isChecked = false;
+
+    const checkbox = document.getElementById('company').addEventListener('change', (e) => {
+        if (e.target.checked) {
+            isChecked = true;
+            companyInfo.style.display = 'block';
+        } else {
+            companyInfo.style.display = 'none';
+        }
+    });
+    
+    const submitBtn = document.getElementById('submit').addEventListener('click', (e) => {
+        e.preventDefault();
+        let formIsValid = true;
+
+        if (!username.value.match(/^[a-zA-Z0-9]{3,20}$/)) {
+            formIsValid = false;
+            username.style.borderColor = 'red';
+        } else {
+            username.style.border = 'none';
+        }
+        if (!email.value.match(/^.*@.*\..*$/)) {
+            formIsValid = false;
+            email.style.borderColor = 'red';
+        } else {
+            email.style.border = 'none';
+        }
+        if (!password.value.match(/^[\w]{5,15}$/) && !confirmPass.value.match(/^[\w]{5,15}$/) || password.value != confirmPass.value) {
+            formIsValid = false;
+            password.style.borderColor = 'red';
+            confirmPass.style.borderColor = 'red';
+        } else {
+            password.style.border = 'none';
+            confirmPass.style.border = 'none';
+        }
+        if (isChecked) {
+            if (!companyNumber.value.match(/^[1-9][0-9]{3}$/)) {
+                companyNumber.style.borderColor = 'red';
+                formIsValid = false;
+            } else {
+                companyNumber.style.border = 'none';
+            }
+        }
+
+        let valid;
+
+        if (formIsValid) {
+            const valid = document.getElementById('valid');
+            valid.style.display = 'block';
+ 
+        } else {
+            valid.style.display = 'none';
+        }
+    });
 }
