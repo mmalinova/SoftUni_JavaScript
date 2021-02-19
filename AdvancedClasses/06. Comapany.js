3d9125fc6b6bb7cdaa08fba4833b555692e1ca0e
mmalinova <malinova29@gmail.com>
2021-02-19 19:14:14 +0200

    Commit some changes at 06. Company.

--- AdvancedClasses/06. Comapany.js
+++ AdvancedClasses/06. Comapany.js
@@ -44,12 +44,12 @@ class Company {
 }
 
 let c = new Company();
-c.addEmployee("Stanimir", 2000, "engineer", "Construction");
-c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
-c.addEmployee("Slavi", 500, "dyer", "Construction");
-c.addEmployee("Stan", 2000, "architect", "Construction");
-c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
-c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
-c.addEmployee("Gosho", 1350, "HR", "Human resources");
+c.addEmployee('Stanimir', 2000, 'engineer', 'Construction');
+c.addEmployee('Pesho', 1500, 'electrical engineer', 'Construction');
+c.addEmployee('Slavi', 500, 'dyer', 'Construction');
+c.addEmployee('Stan', 2000, 'architect', 'Construction');
+c.addEmployee('Stanimir', 1200, 'digital marketing manager', 'Marketing');
+c.addEmployee('Pesho', 1000, 'graphical designer', 'Marketing');
+c.addEmployee('Gosho', 1350, 'HR', 'Human resources');
 console.log(c.bestDepartment());
 
